72d791a0f8fb87a3ed039c7a562dbdf3567297dc
Evan Candler <emcandler21@gmail.com>
2018-04-04 22:30:48 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,7 +8,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :location
       t.integer :costume_inventory
       t.integer :number_of_employees
-      t.string :still_in_business
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
     end
